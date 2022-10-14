205f2c2ec75fa3faa5891fee351ee8a92cf64dda
lathan britz <lathan.britz@drupalconnect.com>
2022-10-13 23:43:20 -0300

    up and running now

--- src/index.js
+++ src/index.js
@@ -1,54 +1,68 @@
 const { XrplClient } = require('xrpl-client')
 const debug = require('debug')
-const messagebird = require('messagebird')(process.env.YOUR-API-KEY)
+const messagebird = require('messagebird')(process.env.YOUR_API_KEY)
 const dotenv = require('dotenv')
 const log = debug('deadman:main')
-const client = new XrplClient([process.env.VALIDATOR-ADMIN-WEBSOCKET])
 
-const isConnected = (async () => {
-    const state = await client.getState()
-    const server_info = await client.send({"id": 1, "command": "server_info"})
-    if (state == null || state.online == false) {
-        return false
+
+class deadman {
+	constructor() {
+        Object.assign(this, {
+            run() {
+                dotenv.config()
+                let last_sent = 0
+                const self = this
+                setTimeout(async () => {
+                    if (last_sent + (process.env.RESEND_TEXT * 1000) > new Date().getTime()) { return }
+            
+                    const result = await self.isConnected()
+                    log('is connected:', result)
+                    if (!result) {
+                        last_sent =  new Date().getTime()
+                        messagebird.messages.create({
+                            originator : process.env.ORIGINATOR,
+                            recipients : [process.env.RECIPIANTS],
+                            body : 'PINEAPPLE PIZZA, your validator is down!'
+                        })
+                        log('sent a text')
+                    }   
+                }, 10000)
+            },
+            async isConnected() {
+                log('checking connection')
+                const client = new XrplClient([process.env.VALIDATOR_ADMIN_WEBSOCKET])
+            
+                const state = await client.getState()
+                const server_info = await client.send({"id": 1, "command": "server_info"})
+                if (state == null || state.online == false) {
+                    return false
+                }
+                
+                if (server_info.info.peers < process.env.PEER_MIN_LIMIT) {
+                    return false
+                }
+            
+                // check a book on the DEX>
+                const asks_books = {
+                    'id': 2,
+                    'command': 'book_offers',
+                    'taker': 'rThREeXrp54XTQueDowPV1RxmkEAGUmg8',
+                    'taker_gets': {'currency': 'USD', 'issuer': 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B' },
+                    'taker_pays': {'currency': 'XRP' }
+                }
+                const book_result = await client.send(asks_books)
+            
+                if ('error' in book_result) {
+                    // this usually traps the limited connection error when a node does not have enough connection
+                    return false
+                }
+            
+                // we are fine and connected
+                return true    
+            }
+        })
     }
-    
-    if (server_info.info.peers < process.env.PEER-MIN-LIMIT) {
-        return false
-    }
+}
 
-    // check a book on the DEX>
-    const asks_books = {
-        'id': 2,
-        'command': 'book_offers',
-        'taker': 'rThREeXrp54XTQueDowPV1RxmkEAGUmg8',
-        'taker_gets': {'currency': 'USD', 'issuer': 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B' },
-        'taker_pays': {'currency': 'XRP' }
-    }
-    const book_result = await client.send(asks_books)
-
-    if ('error' in book_result) {
-        // this usually traps the limited connection error when a node does not have enough connection
-        return false
-    }
-
-    // we are fine and connected
-    return true    
-})
-
-export default (async () => {
-    dotenv.config()
-    let last_sent = 0
-    setTimeout(() => {
-        if (last_sent + (process.env.RESEND-TEXT * 1000) > new Date().getTime()) { return }
-
-        const result = isConnected()
-        if (!result) {
-            last_sent =  new Date().getTime()
-            messagebird.messages.create({
-                originator : process.env.ORIGINATOR,
-                recipients : [process.env.RECIPIANTS],
-                body : 'PINEAPPLE PIZZA, your validator is down!'
-            })
-        }   
-    }, 10000)
-})()
+const main = new deadman()
+main.run()
